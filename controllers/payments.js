3da4e6c8db20671a075f03eaa730d1e735ee2fe2
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-13 18:48:14 +0300

    continue attempts to get the payment to work

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,5 +1,6 @@
 const sendReqToDB = require('../modules/tlg_to_DB')
 const inputLineScene = require('./inputLine')
+const fs = require('fs')
 const GROUP_ID = process.env.GROUP_ID
 require('dotenv').config()
 
@@ -18,18 +19,34 @@ async function paymentScene(bot, msg) {
     const response = JSON.parse(data.ResponseArray[0]);
     const email = response.email;
     const phoneNumber = response.PhoneNumber;
-    //const liqpay = new LiqPay(process.env.LIQPAY_PUBLIC_KEY, process.env.LIQPAY_PRIVATE_KEY)
-    // liqpay.api("request", {
-    //   "action": "invoice_bot",
-    //   "version": "3",
-    //   "email": email,
-    //   "amount": amount,
-    //   "currency": "UAH",
-    //   "order_id": chatId,
-    //   "phone": phoneNumber,
-    // }, function (json) {
-    //   console.log(json.result);
-    // })
+    const filePath = process.env.PATH_FOR_PAYMENTS + chatId + '.html'
+
+    const json_string = {
+      "public_key": process.env.LIQPAY_PUBLIC_KEY,
+      "version": "3",
+      "action": "pay",
+      "amount": amount,
+      "currency": "UAH",
+      "description": email + " " + phoneNumber,
+      "order_id": chatId + "_" + Date.now(),
+      "result_url": "http://silver-service.com.ua/",
+      "server_url": "http://silver-service.com.ua:8000/payment/finish"
+    }
+    const data_string = Buffer.from(JSON.stringify(json_string)).toString('base64')
+    const sign_string = process.env.LIQPAY_PRIVATE_KEY + data_string + process.env.LIQPAY_PRIVATE_KEY
+    const signature = Buffer.from(sign_string).toString('base64')
+    const html = `<form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
+    <input type="hidden" name="data" value="${data_string}"/>
+    <input type="hidden" name="signature" value="${signature}"/>
+    <input type="image" src="//static.liqpay.ua/buttons/p1ru.radius.png"/>
+    </form>`
+    fs.writeFile(filePath, html, function (error) {
+      if (error) {
+        console.error('File saving error:', error);
+      } else {
+        console.log('File saved succesfully.');
+      }
+    })
 
     sendReqToDB('__SaveTlgMsg__', msg.chat, `payment_${chatId}`)
     await bot.sendMessage(chatId, 'Наразі опція оплати послуг знаходиться в стані розробки. Дякуємо за те, що користуєтесь нашими послугами!')
