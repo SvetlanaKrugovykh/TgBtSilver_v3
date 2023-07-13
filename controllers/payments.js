c8862e7aa3388fe96322715132b8aed3b93e7e19
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-13 16:25:51 +0300

    preparing for liqpay

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,23 +1,36 @@
-const LiqPay = require('liqpay')
 const sendReqToDB = require('../modules/tlg_to_DB')
 const inputLineScene = require('./inputLine')
 const GROUP_ID = process.env.GROUP_ID
 require('dotenv').config()
 
-const liqpay = new LiqPay(process.env.LIQPAY_PUBLIC_KEY, process.env.LIQPAY_PRIVATE_KEY)
-
 async function paymentScene(bot, msg) {
   try {
-    const html = liqpay.cnb_form({
-      'action': 'pay',
-      'amount': '1',
-      'currency': 'UAH',
-      'description': 'description text',
-      'order_id': chatId.toSring() + Date.now(),
-      'version': '3'
-    })
-
     const chatId = msg.chat.id
+    await bot.sendMessage(chatId, "Введіть <i>суму оплати в грн без копійок, наприклад введення суми 100 означає 100 гривень </i>, \n", { parse_mode: "HTML" })
+    let userInput = await inputLineScene(bot, msg)
+    let amount = userInput.replace(/[^0-9]/g, "")
+    if (amount.length === 0) {
+      await bot.sendMessage(chatId, "Ви не ввели суму оплати, спробуйте ще раз")
+      return
+    }
+    const jsonString = await sendReqToDB('__GetClientPersData__', msg.chat, `payment_${chatId}`)
+    const data = JSON.parse(jsonString);
+    const response = JSON.parse(data.ResponseArray[0]);
+    const email = response.email;
+    const phoneNumber = response.PhoneNumber;
+    //const liqpay = new LiqPay(process.env.LIQPAY_PUBLIC_KEY, process.env.LIQPAY_PRIVATE_KEY)
+    // liqpay.api("request", {
+    //   "action": "invoice_bot",
+    //   "version": "3",
+    //   "email": email,
+    //   "amount": amount,
+    //   "currency": "UAH",
+    //   "order_id": chatId,
+    //   "phone": phoneNumber,
+    // }, function (json) {
+    //   console.log(json.result);
+    // })
+
     sendReqToDB('__SaveTlgMsg__', msg.chat, `payment_${chatId}`)
     await bot.sendMessage(chatId, 'Наразі опція оплати послуг знаходиться в стані розробки. Дякуємо за те, що користуєтесь нашими послугами!')
 
--- modules/tlg_to_DB.js
+++ modules/tlg_to_DB.js
@@ -4,50 +4,53 @@ const AUTH_TOKEN = process.env.AUTH_TOKEN;
 
 async function sendReqToDB(reqType, data, text) {
 
-	let dataString = objToString(reqType, data, text);
-	console.log(dataString);
+  let dataString = objToString(reqType, data, text);
+  console.log(dataString);
 
-	try {
-		const response = await axios({
-			method: 'post',
-			url: URL,
-			responseType: 'string',
-			headers: {
-				Authorization: `${AUTH_TOKEN}`,
-				'Content-Type': 'application/json',
-			},
-			data: {
-				Query: `Execute;${reqType};${dataString};КОНЕЦ`,
-			}
-		});
+  try {
+    const response = await axios({
+      method: 'post',
+      url: URL,
+      responseType: 'string',
+      headers: {
+        Authorization: `${AUTH_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      data: {
+        Query: `Execute;${reqType};${dataString};КОНЕЦ`,
+      }
+    });
+    if (!response.status == 200) {
+      console.log(response.status)
+      return null
+    } else {
+      if (reqType === '__GetClientPersData__') {
+        return response.data
+      } else {
+        let answer = response.data.toString();
+        console.log(answer.slice(0, 125) + '...');
+        return answer;
+      }
+    }
 
-		if (!response.status == 200) {
-			console.log(response.status);
-			return null;
-		} else {
-			let answer = response.data.toString();
-			console.log(answer.slice(0, 125) + '...');
-			return answer;
-		}
-
-	} catch (err) {
-		console.log(err);
-		return null;
-	}
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
 function objToString(reqType, data, text) {
 
-	switch (reqType) {
-		case '__CheckTlgClient__':
-			return (data.id + '#' + data.first_name + '#' + data.last_name + '#' + data.username);
-		case '___UserRegistration__':
-			return (text + '#' + data?.email + '#' + data?.phoneNumber + '#' + data?.password + '#' + data?.PIB + '#' + data?.contract + '#' + data?.address + '#' + text);
-		case '__GetClientsInfo__':
-			return (text);
-		default:
-			return (data.id + '#' + text);
-	}
+  switch (reqType) {
+    case '__CheckTlgClient__':
+      return (data.id + '#' + data.first_name + '#' + data.last_name + '#' + data.username);
+    case '___UserRegistration__':
+      return (text + '#' + data?.email + '#' + data?.phoneNumber + '#' + data?.password + '#' + data?.PIB + '#' + data?.contract + '#' + data?.address + '#' + text);
+    case '__GetClientsInfo__':
+      return (text);
+    default:
+      return (data.id + '#' + text);
+  }
 }
 
 module.exports = sendReqToDB;
