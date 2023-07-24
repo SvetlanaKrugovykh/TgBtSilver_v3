993c9203e5d55161727e411556e911c934bb3381
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-24 09:25:19 +0300

    fixed test

--- tests/testTelnetOtherOpions.js
+++ tests/testTelnetOtherOpions.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 const telnetCall = require('../modules/telnet');
 const _HOST = '192.168.23.238'
 const EPON = 'EPON0/4:4'
-await telnetCall(_HOST, EPON, 'attenuation')
-  .then(store => {
-    console.dir(store)
-  })
+
+
+async function testTelnetCall(HOST, replaceStr, _conditional = undefined) {
+  await telnetCall(_HOST, EPON, 'attenuation')
+    .then(store => {
+      console.dir(store)
+    })
+}
+
+testTelnetCall(_HOST, EPON, 'attenuation')
