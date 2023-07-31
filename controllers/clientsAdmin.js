4e607a5c0a4d9caca67ea500d61b1c078fe0f397
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-07-31 12:14:40 +0300

    fix bug

--- controllers/clientsAdmin.js
+++ controllers/clientsAdmin.js
@@ -230,6 +230,7 @@ async function clientsAdminStopClientService(bot, msg) {
 }
 
 async function clientsAdminCheckAttenuationService(bot, msg) {
+  const Params = new TelnetParams()
   if (_HOST.length < 12 || Params.excludeHOSTS.includes(_HOST)) {
     await bot.sendMessage(msg.chat.id, "Wrong codeRule. Операцію скасовано. Треба повторити пошук\n", { parse_mode: 'HTML' })
     return null
@@ -247,6 +248,7 @@ async function clientsAdminCheckAttenuationService(bot, msg) {
 }
 
 async function clientsAdminCheckBandWidthService(bot, msg) {
+  const Params = new TelnetParams()
   if (_HOST.length < 12 || Params.excludeHOSTS.includes(_HOST)) {
     await bot.sendMessage(msg.chat.id, "Wrong codeRule. Операцію скасовано. Треба повторити пошук\n", { parse_mode: 'HTML' })
     return null
@@ -264,6 +266,7 @@ async function clientsAdminCheckBandWidthService(bot, msg) {
 }
 
 async function clientsAdminCheckMacsService(bot, msg) {
+  const Params = new TelnetParams()
   if (_HOST.length < 12 || Params.excludeHOSTS.includes(_HOST)) {
     await bot.sendMessage(msg.chat.id, "Wrong codeRule. Операцію скасовано. Треба повторити пошук\n", { parse_mode: 'HTML' })
     return null
