fd532fa9e68d85c298b88874407b78ccdec5656a
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-09-14 09:27:00 +0300

    correct mailer.js

--- modules/mailer.js
+++ modules/mailer.js
@@ -2,31 +2,35 @@ const nodemailer = require('nodemailer')
 const { MAILHOST, MAILPORT } = process.env
 
 async function sendMail(message, filename) {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: MAILHOST,
+      port: Number(MAILPORT),
+      secure: false,
+      auth: {
+        user: message.from,
+        pass: undefined
+      }
+    })
 
-  let transporter = nodemailer.createTransport({
-    host: MAILHOST,
-    port: Number(MAILPORT),
-    secure: false,
-    auth: {
-      user: message.from,
-      pass: undefined
+    const attachment = {
+      filename: 'receipt.pdf',
+      path: filename
     }
-  })
 
-  const attachment = {
-    filename: 'receipt.pdf',
-    path: filename
+    message.attachments = [attachment]
+    if (process.env.MAIL_TEST === 'true') {
+      message.to = process.env.MAIL_TEST_TO
+    }
+
+    let info = await transporter.sendMail(message)
+
+    console.log("Message sent: %s", info.messageId)
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+  } catch (error) {
+    console.error(error)
   }
 
-  message.attachments = [attachment]
-  if (process.env.MAIL_TEST === 'true') {
-    message.to = process.env.MAIL_TEST_TO
-  }
-
-  let info = await transporter.sendMail(message)
-
-  console.log("Message sent: %s", info.messageId)
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
 }
 
 module.exports = { sendMail }
