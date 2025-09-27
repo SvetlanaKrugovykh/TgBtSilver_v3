05287975407ccec93266a0fda9c276427c9e94c8
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-09-27 14:31:13 +0200

    some cleaning up

--- controllers/payment.controller.js
+++
@@ -1,8 +0,0 @@
-const HttpError = require('http-errors')
-require('dotenv').config()
-
-module.exports.getPaymentLink = async function (request, reply) {
-  // const { reqType, device_ip, epon_id } = request.body
-
-
-}
